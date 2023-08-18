b908ab5ed8e22f4a12ce97b8585e89aaabf6b367
mHonc <michal.honc123@gmail.com>
2023-08-18 18:20:35 +0200

    Added opening google page

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,16 +1,17 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-    public static void main(String[] args) {
-
-        WebDriver driver = new ChromeDriver();
-    }
-
     @Test
-    public void sampleTest() {
-        System.out.println("Test");
+    public void openGooglePage() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.pl/?hl=pl");
+
+        WebDriver driver2 = new FirefoxDriver();
+        driver2.get("https://www.google.pl/?hl=pl");
     }
+
 }
