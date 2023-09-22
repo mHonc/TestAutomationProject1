f86b6d0db34c111066ddf31d987665cc4af2d1c7
mHonc <michal.honc123@gmail.com>
2023-09-22 13:19:55 +0200

    Added taking screenshot

--- src/test/java/LoadFileTest.java
+++ src/test/java/LoadFileTest.java
@@ -1,17 +1,22 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class LoadFileTest {
 
     @Test
-    public void load(){
+    public void load() throws IOException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
         driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\M\\IdeaProjects\\TestAutomationProject1\\testowy.txt");
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File file = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(file, new File("resources/screenshot.png"));
     }
 }
